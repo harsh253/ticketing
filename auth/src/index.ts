567a2355e5bd73a9723223712804a5a006b68559
harsh253 <harshjaiswal253@gmail.com>
2021-09-04 21:18:49 +0530

    setup jest framework

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,14 @@
-import cookieSession from "cookie-session";
-import express from "express";
-import "express-async-errors";
+import { app } from "./app";
 import { initDB } from "./config/db";
-import { NotFoundError } from "./error/NotFoundError";
-import { errorHandler } from "./middlewares/errorHandler";
-import { currentUserRouter } from "./routes/current-user";
-import { signInRouter } from "./routes/signin";
-import { signOutRouter } from "./routes/signout";
-import { signUpRouter } from "./routes/signup";
 
-const app = express();
-app.set("trust proxy", true);
-app.use(
-  cookieSession({
-    signed: false, // we don't need encryption here
-    secure: true,
-  })
-);
-app.use(express.json());
 const PORT = 3000;
 
-const routes = [currentUserRouter, signInRouter, signOutRouter, signUpRouter];
+const initApp = () => {
+  initDB();
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY (jwt-secret-key) is not added");
+  }
+  app.listen(PORT, () => console.log(`Running auth service on port - ${PORT}`));
+};
 
-routes.forEach((route) => {
-  app.use(route);
-});
-
-app.all("*", () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
-
-initDB();
-
-app.listen(PORT, () => console.log(`Running auth service on port - ${PORT}`));
+initApp();
