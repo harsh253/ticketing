b3d5b64c00a4765c73ea29bbb3ac6073e82c3de4
harsh253 <harshjaiswal253@gmail.com>
2021-09-05 22:17:36 +0530

    nextjs infra setup

---
+++ client/next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // Adding so that skaffold polls for nextjs changes every 300 seconds
+    webpackDevMiddleware: (config) => {
+        config.watchOptions.poll = 300;
+        return config;
+    }
+}
